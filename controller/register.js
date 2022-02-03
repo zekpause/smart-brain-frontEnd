0f6df638a282c1341d764fcca4661f8e5aa1cda7
zekpause <samson6603@gmail.com>
2022-02-03 20:12:41 +0800

    maybe

--- controller/register.js
+++ controller/register.js
@@ -1,6 +1,6 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    const { name, email, password } = req.body;
-    if (!email || !password) {
+    const { email, name, password } = req.body;
+    if (!email || !name || !password) {
         return res.status(400).json('incorrect submission form')
     }
     const hash = bcrypt.hashSync(password);
